c409e422aa80cb165124a5e53cce5c88e7a13d81
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-20 18:28:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog 
 end 
 
-def bark
-  puts "Woof!"
-end
-
-def sits 
-  puts "The Dog is sitting"
-end
+  def bark
+    puts "Woof!"
+  end
+  
+  def sits 
+    puts "The Dog is sitting"
+  end
 
 fido = Dog.new
 fido.bark
